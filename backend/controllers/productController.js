f014a4a2f2a6f914e9fd22c6c3419ec906f2d855
Thevindu-Senanayake <thevindusenanayake76@gmail.com>
2021-12-05 07:20:12 +0530

    Feature Added: delete product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require('../models/product');
 
 // Create new product  => /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
+
 	const product = await Product.create(req.body)
 
 	res.status(201).json({
@@ -39,6 +40,7 @@ exports.getSingleProduct = async (req, res, next) => {
 		product
 	})
 }
+
 // Update Product  => /api/v1/admin/product/:productId
 exports.updateProduct = async (req, res, next) => {
 
@@ -59,5 +61,25 @@ exports.updateProduct = async (req, res, next) => {
 	res.status(200).json({
 		success: true,
 		product
+	})
+}
+
+// Delete Product  => /api/v1/admin/product/:productId
+exports.deleteProduct = async (req, res, next) => {
+
+	const product = await Product.findById(req.params.productId);
+
+	if(!product) {
+		return res.status(404).json({
+			success: false,
+			message: 'Product not found'
+		})
+	}
+
+	await product.deleteOne();
+
+	res.status(200).json({
+		success: true,
+		message: 'Product is deleted'
 	})
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,12 +3,21 @@ const { routes } = require('../app');
 const router = express.Router();
 
 
-const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController');
+const {
+	getProducts,
+	newProduct,
+	getSingleProduct,
+	updateProduct,
+	deleteProduct
+
+} = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
 router.route('/product/:productId').get(getSingleProduct);
 
 router.route('/adminproduct/new').post(newProduct);
-router.route('/admin/product/:productId').put(updateProduct);
+router.route('/admin/product/:productId')
+	.put(updateProduct)
+	.delete(deleteProduct);
 
 module.exports = router;
